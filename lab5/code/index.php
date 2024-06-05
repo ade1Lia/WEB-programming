0357b999857a8eadcf9e9483db48dd598b6dbe4e
Adelia <adelia.231.nat@gmail.com>
2024-06-05 22:01:16 +0200

    Update index.php

--- lab5/code/index.php
+++ lab5/code/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Куликова Полина</title>
+    <title>Замулдинова Адэля</title>
 </head>
 <body>
 <form action="index2.php" method="post">
